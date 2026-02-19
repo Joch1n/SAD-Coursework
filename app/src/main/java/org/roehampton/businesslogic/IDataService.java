175db492dfc75795142f6feb59e70809610f8833
lybaa786 <lybaa611@gmail.com>
2026-02-19 23:38:03 +0000

    updated presentation layer

--- app/src/main/java/org/roehampton/businesslogic/IDataService.java
+++ app/src/main/java/org/roehampton/businesslogic/IDataService.java
@@ -1,15 +1,9 @@
 package org.roehampton.businesslogic;
 
+import org.roehampton.domain.PriceSeries;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface IDataService {
-
-    /**
-     * Returns chart-ready data for selected companies and date range.
-     */
-    ChartDataDTO getChartData(List<String> symbols,
-                              LocalDate startDate,
-                              LocalDate endDate);
+    PriceSeries getSharePrices(String symbol, LocalDate from, LocalDate to);
 }
